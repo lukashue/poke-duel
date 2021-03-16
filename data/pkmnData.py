735a6e70e078fc09ff0cc95b5eb3404b2004d021
Lukas Hültenschmidt <lhueltenschmidt@gmail.com>
2021-03-16 16:13:33 +0100

    pokemon species are now realised as instances of the pokemon-class, sorted some data

---
+++ data/pkmnData.py
@@ -0,0 +1,29 @@
+import json
+
+# This script serves as a generator for the corresponding JSON file
+# I use it because some huge changes in the JSON file can be easily generated in here
+# If you want to edit the stats of some Pokemon, change them here and run the script.
+
+pkmnData = {
+  "charmander": {
+    "base_stats": {
+      'hp': 39, 'atk': 52, 'def': 43, 'spa': 60, 'spd': 50, 'spe': 65
+    },
+    "type": ["Fire", None],
+    "learnset": {
+      'scratch': 1, 'ember': 4 
+    }   
+  },
+  "squirtle": {
+    "base_stats": {
+      'hp': 44, 'atk': 48, 'def': 65, 'spa': 50, 'spd': 64, 'spe': 43
+    },
+    "type": ["Water", None],
+    "learnset": {
+      'tackle': 1, 'water_gun': 3
+    }   
+  }
+}
+
+with open('pkmnData.json', "w") as file:
+  json.dump(pkmnData, file, indent=2)
