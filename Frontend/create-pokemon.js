b7ed1b4ed315161ccfbe25c11c53a3c699420521
Lukas Hültenschmidt <lhueltenschmidt@gmail.com>
2021-04-21 19:53:08 +0200

    multiple components now work at the same time, still using vanilla JS, may rewrite this with REACT or similar

--- Frontend/create-pokemon.js
+++ Frontend/create-pokemon.js
@@ -2,11 +2,11 @@ const imgBaseURL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 const pkmnImg = document.querySelector("#pkmn-img");
-const pkmnSelector = document.querySelector("#pkmn-selector");
+const pkmnSelectors = document.querySelectorAll(".pkmn-selector");
 const type1 = document.querySelector("#type1");
 const type2 = document.querySelector("#type2");
-const levelCounter = document.querySelector("#levelCount");
-const levelRange = document.querySelector("#levelRange");
+const levelCounters = document.querySelectorAll(".levelCount");
+const levelRanges = document.querySelectorAll(".levelRange");
 
 function capitalize(string) {
   return string[0].toUpperCase() + string.substring(1);
@@ -16,31 +16,43 @@ function stripLeftZeroes(string) {
   return string.match(regex)[0];
 }
 
-levelRange.oninput = function () {
-  levelCounter.value = this.value;
-};
-levelCounter.onchange = function () {
-  levelRange.value = this.value;
-};
-pkmnSelector.onchange = function () {
-  let pkmn = this.value;
-  fetch("../data/pkmnData.json")
-    .then((response) => response.json())
-    .then((data) => {
-      let [primary, secondary] = data[pkmn].type;
-      let imgURL = imgBaseURL + stripLeftZeroes(data[pkmn].no) + ".png";
-      console.log(secondary);
+for (let levelRange of levelRanges) {
+  levelRange.oninput = function () {
+    let levelCounter = levelRange.nextSibling.nextSibling;
+    levelCounter.value = this.value;
+  };
+}
+for (let levelCounter of levelCounters) {
+  levelCounter.onchange = function () {
+    let levelRange = levelCounter.previousSibling.previousSibling;
+    levelRange.value = this.value;
+  };
+}
+for (pkmnSelector of pkmnSelectors) {
+  pkmnSelector.onchange = function () {
+    let pkmn = this.value;
+    fetch("../data/pkmnData.json")
+      .then((response) => response.json())
+      .then((data) => {
+        let [primary, secondary] = data[pkmn].type;
+        let imgURL = imgBaseURL + stripLeftZeroes(data[pkmn].no) + ".png";
 
-      pkmnImg.src = imgURL;
+        let siblings = this.parentElement.parentElement.children;
+        let pkmnImg = siblings[0];
+        let type1 = siblings[1].children[1].children[0];
+        let type2 = siblings[1].children[1].children[1];
 
-      type1.innerText = capitalize(primary);
-      type1.setAttribute("class", primary);
-      if (secondary) {
-        type2.innerText = capitalize(secondary);
-        type2.setAttribute("class", secondary);
-      } else {
-        type2.innerText = "";
-        type2.setAttribute("class", "hidden");
-      }
-    });
-};
+        pkmnImg.src = imgURL;
+
+        type1.innerText = capitalize(primary);
+        type1.setAttribute("class", primary);
+        if (secondary) {
+          type2.innerText = capitalize(secondary);
+          type2.setAttribute("class", secondary);
+        } else {
+          type2.innerText = "";
+          type2.setAttribute("class", "hidden");
+        }
+      });
+  };
+}
